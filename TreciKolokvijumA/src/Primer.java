6899bd4168aef78d95b6f7edfc70e47721d667ca
Kristina <trninickristina42@gmail.com>
2018-01-17 17:37:29 +0100

    Vracen indeks umesto vrednosti

--- TreciKolokvijumA/src/Primer.java
+++ TreciKolokvijumA/src/Primer.java
@@ -14,10 +14,10 @@ public class Primer {
 	public int findLast(int[] x, int y) {
 		for (int i = x.length - 1; i > 0; i--) {
 			if (x[i] == y) {
-				return y;
+				return-1;
 			}
 		}
-		return -1;
+		return y;
 	}
 	// test: x = [2, 3, 5]; y = 2; Expected = 0
 
